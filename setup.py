5824eb769e81844e813b487bb7579616ddcd0c63
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-12-01 12:00:24 +0000

    Include numpy headers

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import numpy
 
 setup(name             = "permanent",
-      version          = "0.1.2",
+      version          = "0.1.3",
       description      = "Calculates the permanent of a Numpy matrix",
       author           = "Pete Shadbolt",
       author_email     = "pete.shadbolt@gmail.com",
@@ -13,7 +14,8 @@ setup(name             = "permanent",
       ext_modules      = [
           Extension(
               'permanent.permanent', ['./src/permanent.c'],
-              extra_compile_args=["-Ofast", "-march=native"]),
+              extra_compile_args=["-Ofast", "-march=native"],
+              include_dirs=[numpy.get_include()]),
       ], 
       
 )
