f2e8ad9f009de9953a5dcdb38cb39a87b8259eaa
droodev <droodev@gmail.com>
2024-10-28 12:08:10 -0500

    Fixing the python and setuptools part.

--- permanent/__init__.py
+++
@@ -1,1 +0,0 @@
-from permanent import permanent
--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup, Extension
 import numpy
 
 setup(
-    name="permanent",
-    version="0.1.4",
-    description="Calculates the permanent of a Numpy matrix",
-    author="Pete Shadbolt",
-    author_email="hello@peteshadbolt.co.uk",
-    maintainer="hello@peteshadbolt.co.uk",
-    url="https://github.com/peteshadbolt/permanent",
-    packages=["permanent"],
-    setup_requires=["numpy"],
     ext_modules=[
         Extension(
-            'permanent.permanent', ['./src/permanent.c'],
+            'permanentbis.permanent', ['./src/permanent.c'],
             extra_compile_args=["-Ofast", "-march=native"],
             include_dirs=[numpy.get_include()]),
     ],
