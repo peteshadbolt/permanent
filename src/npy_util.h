a0e936861acbdb9e942e1fe911745a54f3af3c5e
droodev <droodev@gmail.com>
2024-10-28 12:09:02 -0500

    Made the C part using the new matplotlib's convention of using complex.h in their libs.

--- src/npy_util.h
+++ src/npy_util.h
@@ -5,43 +5,31 @@
 #define SM_shape(x0) (int) PyArray_DIM(submatrix, x0)
 
 // Complex numbers
-static const npy_complex128 complex_one = {.real=1, .imag=0};
-static const npy_complex128 complex_zero = {.real=0, .imag=0};
+static const npy_complex128 complex_one = 1 + 0 * I;
+static const npy_complex128 complex_zero = 0 + 0 * I;
 
 // Add two numbers
 npy_complex128 complex_add(npy_complex128 a, npy_complex128 b) { 
-    npy_complex128 x;
-    x.real = a.real+b.real;
-    x.imag = a.imag+b.real;
-    return x;
+    return a+b;
 }
 
 // Product of two numbers
 npy_complex128 complex_prod(npy_complex128 a, npy_complex128 b) { 
-    npy_complex128 x;
-    x.real = a.real*b.real - a.imag*b.imag;
-    x.imag = a.imag*b.real + a.real*b.imag;
-    return x;
+    return a*b;
 }
 
 // Product of complex and float
 npy_complex128 complex_float_prod(npy_complex128 a, float b) { 
-    npy_complex128 x;
-    x.real = a.real*b;
-    x.imag = a.imag*b;
-    return x;
+    return a*b;
 }
 
 // Increment a number
 void complex_inc(npy_complex128 *a, npy_complex128 b) { 
-    a->real += b.real;
-    a->imag += b.imag;
+    *a += b;
 }
 
 // Multipy a number by another one
 void complex_multiply(npy_complex128 *a, npy_complex128 b) { 
-    double c = a->real;
-    a->real  = a->real*b.real-a->imag*b.imag;
-    a->imag  = c*b.imag+a->imag*b.real;
+    *a *= b;
 }
 
